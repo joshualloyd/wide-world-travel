41417a7d58ed60fdc763d4d08deef0e65f97999b
Joshua Lloyd <joshualloyd@joshualloyd.com>
2017-07-25 21:44:28 -0500

    added factory

--- app/controllers/bookCtrl.js
+++ app/controllers/bookCtrl.js
@@ -1,21 +1,8 @@
 'use strict';
 
-travelApp.controller('bookCtrl', function($scope, $http, $q){
-	$scope.books;
+travelApp.controller('bookCtrl', function($scope, GuideFactory){
 
-	let getBooks = () => {
-		return $q((resolve, reject)=>{
-			$http.get('../../data/guides.json')
-			.then((guides)=>{
-				resolve(guides);
-			})
-			.catch((err)=>{
-				console.log('that guides xhr went badly', err);
-			});
-		});
-	};
-
-	getBooks()
+	GuideFactory.getBooks()
 	.then((booksData)=>{
 		console.log('booksData', booksData.data.guides);
 		$scope.books = booksData.data.guides;
